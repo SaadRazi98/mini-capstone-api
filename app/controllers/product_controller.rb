7204034984eabbc6e88c8e67de4f8504f3498665
SAAD <SAADRAZI1998@GMAIL.com>
2023-03-30 16:05:14 -0500

    New product Kang

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -20,6 +20,10 @@ class ProductController < ApplicationController
       image_url: params[:image_url]
       description: params[:description]
     )
+
+    @product = Product.new(
+      name: "Kang the Conqueror Action Figure", price: 25, image_url: "https://cdn.shopify.com/s/files/1/0169/6995/7440/products/F6575_PROD_MVL_AMN_LEGENDS_FROST_3_69161_Black_Online_2000SQ_1500x.jpg?v=1675438086", description: "Kang is the main villain for the MCU. This action figure he has his famous green and purple suit with his blue helmet shield."
+    )
     @product.save
     render :show
   end 
@@ -40,5 +44,5 @@ class ProductController < ApplicationController
     @product.destroy
     render json:{message: "Product has been successfully removed"}
   end 
-  
+
 end 
