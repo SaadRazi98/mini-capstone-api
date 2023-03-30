fa3dbc4f747387f95f743c92321618100ff8ff35
SAAD <SAADRAZI1998@GMAIL.com>
2023-03-30 12:49:00 -0500

    controller and routes update 2

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,4 +3,13 @@ class ProductController < ApplicationController
   def index
     @product = Product.all 
     render template: "product/index"
-end
+  end 
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render :show
+  end 
+
+  def create
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get "/product/:jakesully" => "product#index"
+  
+get "/products" => "product#index"
+get "/products/:id" => "products#show"
+post "/products" => 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
